7ff70472d03fa4e4aa30ed15751b3874f9960f35
Mert Celil <mertcelilozdin@hotmail.com>
2021-09-04 14:57:01 +0300

    readme and some files updated

--- app/features/auth/screens/login/controller.js
+++ app/features/auth/screens/login/controller.js
@@ -1,21 +1,8 @@
 import ROUTE_NAMES from "../../../../navigation/config/route-names.json";
 
-const initialState = {
-  initialized: false
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "setInitialized":
-      return { ...state, initialized: action.payload };
-    default:
-      return state;
-  }
-};
-
-const handleLoginSubmit = async (navigation, accountContext) => {
+function handleLoginSubmit(navigation, accountContext) {
   accountContext.setAccount({ username: "johndoe", token: null });
   navigation.replace(ROUTE_NAMES.MAIN_TAB);
-};
+}
 
-export { initialState, reducer, handleLoginSubmit };
+export { handleLoginSubmit };
--- app/features/profile/screens/main/controller.js
+++ app/features/profile/screens/main/controller.js
@@ -1,21 +1,8 @@
 import ROUTE_NAMES from "../../../../navigation/config/route-names.json";
 
-const initialState = {
-  initialized: false
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "setInitialized":
-      return { ...state, initialized: action.payload };
-    default:
-      return state;
-  }
-};
-
-const handleLogoutSubmit = async (navigation, accountContext) => {
+function handleLogoutSubmit(navigation, accountContext) {
   accountContext.clearAccount();
   navigation.replace(ROUTE_NAMES.AUTH_STACK);
-};
+}
 
-export { initialState, reducer, handleLogoutSubmit };
+export { handleLogoutSubmit };
